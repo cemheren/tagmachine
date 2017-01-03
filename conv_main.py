669ec3903a59b9bba204ce0befd597d19751df61
cemheren <cemheren@gmail.com>
2017-01-02 22:01:47 -0800

    reduce number of epochs

--- conv_main.py
+++ conv_main.py
@@ -81,7 +81,7 @@ m = tflearn.DNN(network, clip_gradients=5.0)
 
 print("starting training.")
 
-for i in range(50):
+for i in range(30):
     m.fit(trainX, trainY, validation_set=0.1, shuffle=True, show_metric=True, batch_size=32, n_epoch=10, run_id=str(i))
     print("-- TESTING...")
     q = m.predict(np.reshape(trainX[0], (1, 120)))[0]
--- lstm_main.py
+++ lstm_main.py
@@ -71,7 +71,7 @@ m = tflearn.DNN(g, clip_gradients=5.0)
 
 print("starting training.")
 
-for i in range(50):
+for i in range(30):
     m.fit(trainX, trainY, validation_set=0.1, show_metric=True, batch_size=32, n_epoch=2, run_id=str(i))
     print("-- TESTING...")
     q = m.predict(np.reshape(trainX[0], (1, 120)))[0]
